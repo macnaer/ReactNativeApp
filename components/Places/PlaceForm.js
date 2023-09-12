cd76e345b26a6c7a614e7460c6bdc30bef1a27c3
trofimchuk <trofimchuk@itstep.academy>
2023-09-12 19:07:43 +0300

    Add styles for form

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -1,13 +1,48 @@
-import { Text, View } from "react-native";
+import { ScrollView, Text, TextInput, View } from "react-native";
 import ImagePicker from "../Places/ImagePicker";
+import { useState } from "react";
 
 function PlaceForm() {
+  const [enteredTitle, setEnteredTitle] = useState("");
+
+  function changeTitleHandler(text) {
+    setEnteredTitle(text);
+  }
+
   return (
-    <View>
-      <Text>Place form</Text>
-      <ImagePicker />
-    </View>
+    <ScrollView style={styles.form}>
+      <View>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          value={enteredTitle}
+          style={styles.input}
+          onChange={changeTitleHandler}
+        />
+        <ImagePicker />
+      </View>
+    </ScrollView>
   );
 }
 
 export default PlaceForm;
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  label: {
+    fontWeight: "bold",
+    marginBottom: 4,
+    color: "blue",
+  },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 4,
+    fontSize: 16,
+    borderBottomColor: "pink",
+    borderBottomWidth: 2,
+    backgroundColor: "gray",
+  },
+});
